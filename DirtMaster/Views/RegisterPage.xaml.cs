26b97bda89c932957aeb0cc5f68c6cace84f847a
Mikul58 <3tcmikulski@gmail.com>
2021-09-23 20:59:56 +0200

    Added LoginPage and RegisterPage. and edited LoginOrRegisterTabbedPage Added entry and label style in App.Xaml.

--- DirtMaster/Views/RegisterPage.xaml.cs
+++ DirtMaster/Views/RegisterPage.xaml.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace DirtMaster.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class LoginPage : ContentPage
+    public partial class RegisterPage : ContentPage
     {
-        public LoginPage()
+        public RegisterPage()
         {
             InitializeComponent();
         }
